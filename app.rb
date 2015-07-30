4d26305f6b9a59d2a524ad43be989e6cba9a0653
RadWells <scwells@icloud.com>
2015-07-30 15:45:49 -0400

    Search logic working

--- app.rb
+++ app.rb
@@ -7,12 +7,17 @@ ActiveRecord::Base.establish_connection({
 
 require_relative 'models/lawyer'
 
-get '/lawyers' do
-  @lawyers = Lawyer.all
+get '/' do
   erb :index
 end
 
-get '/lawyers/:id' do
-  @lawyer = Lawyer.find(params[:id])
+#get '/lawyers/:id' do
+# @lawyer = Lawyer.find(params[:id])
+ #erb :show
+ #end
+
+get '/lawyers' do
+  #city = params['city_name']
+  @lawyers = Lawyer.where(city: params[:city_name])
   erb :show
-end
+ end
